47d82d2184a0fd05974bdcf0c187713127b89475
alia.abdrahman <alia.abdrahman@setel.com>
2023-08-28 10:10:56 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SearchScreen from './screens/SearchScreen';
+import { AntDesign } from '@expo/vector-icons';
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+        tabBarActiveTintColor: "green",
+      }}>
+        <Tab.Screen name="Home" component={HomeScreen} options={{
+          tabBarIcon: () => <AntDesign name="home" size={24} color="black" />
+        }} />
+        <Tab.Screen name="Search" component={SearchScreen} options={{
+          tabBarIcon: () => <AntDesign name="search1" size={24} color="black" />
+        }} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
---
+++ screens/HomeScreen.js
@@ -0,0 +1,67 @@
+import React, {useEffect, useState} from "react";
+import {View,StyleSheet, Text, SafeAreaView,FlatList} from "react-native";
+import Article from "../components/Article";
+import axios from "axios";
+
+const HomeScreen = () => {
+
+const [articles,setArticles] = useState([]);
+
+const getNews = () => {
+    axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=14bc8142a28d46c3a4647a6f54d8c347',
+    {
+            params:{
+            category: "science"
+        }
+
+    })
+        .then((response) =>{
+            // handle success
+            console.log(response.data.articles);
+            setArticles(response.data.articles);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
+}
+
+
+useEffect(() => {
+    getNews();
+}, []); // empty depency array 
+
+
+
+return(
+    <SafeAreaView style={styles.container}>
+        <FlatList
+            data={articles} 
+            renderItem = {({item}) =>
+                <Article
+                    urlToImage = {item.urlToImage}
+                    title = {item.title}
+                    description = {item.description}
+                    author = {item.author}
+                    publishedAt = {item.publishedAt}
+                    sourceName = {item.source.name}
+                />}
+
+            keyExtractor = {(item) => item.title}
+        />
+        
+    </SafeAreaView>
+       
+    )
+}
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container:{
+
+    }
+})
